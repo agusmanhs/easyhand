ef589f8100a9e83ab9503b6062e9426e69a376b0
Agusman Haryadi <agusman.h.s@gmail.com>
2026-07-26 07:50:35 +0800

    fix: enforce strict role-based access control for filament panels

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,15 +9,29 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
 use Spatie\Permission\Traits\HasRoles;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, HasRoles;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if ($panel->getId() === 'admin') {
+            return $this->hasRole('admin');
+        }
+
+        if ($panel->getId() === 'member') {
+            return $this->hasRole('member');
+        }
+
+        return false;
+    }
 
 
     /**
