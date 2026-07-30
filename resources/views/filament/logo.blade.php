f5e4f1a225e2eccfc1e5165ec24139409b3e3ea4
Agusman Haryadi <agusman.h.s@gmail.com>
2026-07-31 04:10:01 +0800

    Update logo typography to match official brand guidelines

--- resources/views/filament/logo.blade.php
+++ resources/views/filament/logo.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center gap-2">
-    <img src="{{ asset('images/easyhand-logo.svg') }}" alt="EasyHand Logo" class="h-8 w-8 object-contain" />
-    <span class="text-xl font-bold tracking-tight text-gray-950 dark:text-white" style="line-height: 1;">
-        Easy<span class="text-primary-600">Hand</span>
+    <img src="{{ asset('images/easyhand-logo.svg') }}" alt="easyhand Logo" class="h-8 w-8 object-contain" />
+    <span class="text-2xl font-bold tracking-tight text-[#2d3338] dark:text-white" style="line-height: 1; font-family: 'Plus Jakarta Sans', sans-serif;">
+        easy<span style="color: #ff6000;">hand</span>
     </span>
 </div>
