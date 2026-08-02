d2cbf0872f25278494491ab428ea62a2e4708d19
Agusman Haryadi <agusman.h.s@gmail.com>
2026-08-03 02:36:00 +0800

    Fix dark mode logo visibility using pure CSS

--- resources/views/filament/logo.blade.php
+++ resources/views/filament/logo.blade.php
@@ -1,6 +1,12 @@
+<style>
+    .fi-logo-light { display: block; }
+    .fi-logo-dark { display: none; }
+    html.dark .fi-logo-light { display: none !important; }
+    html.dark .fi-logo-dark { display: block !important; }
+</style>
 <div class="flex items-center">
     <!-- Light Mode Logo -->
-    <img src="{{ asset('images/easyhand-full-logo.png') }}" alt="easyhand" class="h-10 w-auto object-contain dark:hidden" />
+    <img src="{{ asset('images/easyhand-full-logo.png') }}" alt="easyhand" class="fi-logo-light h-10 w-auto object-contain" />
     <!-- Dark Mode Logo -->
-    <img src="{{ asset('images/easyhand-logo-dark.png') }}" alt="easyhand" class="hidden h-10 w-auto object-contain dark:block" />
+    <img src="{{ asset('images/easyhand-logo-dark.png') }}" alt="easyhand" class="fi-logo-dark h-10 w-auto object-contain" />
 </div>
