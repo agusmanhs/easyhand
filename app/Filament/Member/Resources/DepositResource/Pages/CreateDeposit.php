56a6a5e9536e9f195a19c8510f72fdaf67ba2a52
Agusman Haryadi <agusman.h.s@gmail.com>
2026-08-03 23:43:09 +0800

    Add payment method name to deposit telegram notification

--- app/Filament/Member/Resources/DepositResource/Pages/CreateDeposit.php
+++ app/Filament/Member/Resources/DepositResource/Pages/CreateDeposit.php
@@ -33,6 +33,7 @@ class CreateDeposit extends CreateRecord
             
             $msg = "<b>💰 Request Deposit Baru!</b>\n\n"
                  . "<b>Member:</b> {$user->name} ({$user->email})\n"
+                 . "<b>Metode:</b> {$deposit->paymentMethod->name}\n"
                  . "<b>Nominal:</b> Rp " . number_format($deposit->amount, 0, ',', '.') . "\n"
                  . "<b>Kode Unik:</b> {$deposit->unique_code}\n"
                  . "<b>Total Transfer:</b> Rp " . number_format($deposit->total_transfer, 0, ',', '.') . "\n"
